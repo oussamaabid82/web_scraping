0091bedc87d7665732c4dfde0f8190dd8b38c8db
Oussama Abid <oussamaabidd@gmail.com>
2021-07-16 09:48:12 +0100

    finalisation du programme qui scrape une page

--- info_category.py
+++ info_category.py
@@ -2,16 +2,28 @@ import infos_page as ip
 import csv
 
 def get_books_links(result_fetch_categori):
-    books_links = ip.page_scraper(result_fetch_categori).find('ol', {'class': 'row'})('a')
+    link_categori = [result_fetch_categori[1]]
+    if (ip.page_scraper(result_fetch_categori).find('li', {'class': 'next'}).text) == "next":
+        page_number = ip.page_scraper(result_fetch_categori).find('li', {'class': 'next'}).a['href']
+        link_categori.append(result_fetch_categori[1].replace('index.html', page_number))
     links = []
-    for i in books_links:
-        links.append("https://books.toscrape.com/catalogue/" + i['href'].replace('../', ''))
-    for link in links:
-        return (str(link))
+    for link in link_categori:
+        resut_fetch = ip.fetch(link)
+        books_links = ip.page_scraper(resut_fetch).find('ol', {'class': 'row'})('h3')
+        for book_link in books_links:      
+            links.append("https://books.toscrape.com/catalogue/" + book_link.a['href'].replace('../', ''))
+    return (links)
+   
+def get_infos_book(fetch):
+    for link in fetch:
+        result_fetch_book = ip.fetch(link)
+        recups = ip.recup(result_fetch_book)
+        return recups
 
 if __name__ =='__main__':    
-    result_request_categori = ip.fetch("https://books.toscrape.com/catalogue/category/books/travel_2/index.html")
+    result_request_categori = ip.fetch("https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html")
     links_books = get_books_links(result_request_categori)
-    result_fetch_books = ip.fetch(links_books)
-    recups = ip.recup(result_fetch_books)
-    ip.info_page_csv(recups)
+    file_name = input("nomme le fichier: ")
+    recups = get_infos_book(links_books)
+    ip.creating_csv_file(recups,file_name)
+    
--- infos_page.py
+++ infos_page.py
@@ -2,14 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-link = []
 def fetch(url):
     result = requests.get(url)
-    link.append(url)
-    return result
+    return (result , url)
     
 def page_scraper(result_fetch):
-    soup = BeautifulSoup(result_fetch.content, 'html.parser')
+    soup = BeautifulSoup((result_fetch[0]).content, 'html.parser')
     return soup
 
 def affichage_link(url):
@@ -45,17 +43,17 @@ def get_product_category(result_fetch):
 
 def get_review_rating(result_fetch):    
     rev_rating = page_scraper(result_fetch).find('p', {'class': 'star-rating'})
-    return (str(rev_rating.text))
+    return (rev_rating.attrs.get('class')[1])
 
 def get_product_pic(result_fetch):
     product_img = page_scraper(result_fetch).find('div', {'class': 'item active'})('img')
     links_pic=[]
     for i in product_img:
         links_pic.append("https://books.toscrape.com/" + i['src'].replace('../', ''))
-    return (str(links_pic))
+    return (links_pic[0])
     
 def recup(result_fetch):
-    affichage_url = affichage_link(link)
+    affichage_url = affichage_link(result_fetch[1])
     pro_upc = get_universal_product_code(result_fetch)
     pro_titel = get_product_titel(result_fetch)
     pro_price_tax_inc = get_price_including_tax(result_fetch)
@@ -66,18 +64,19 @@ def recup(result_fetch):
     pro_rating = get_review_rating(result_fetch)
     pro_picture = get_product_pic(result_fetch)
     all_program = [affichage_url, pro_upc, pro_titel, pro_price_tax_inc, pro_price_tax_exc, pro_num_available, pro_description, pro_category, pro_rating, pro_picture]
-    return (all_program)
+    return all_program
 
-def info_page_csv(program):
+def creating_csv_file(program, file_name):
     ligne_en_tete = ['product_page_url', 'universal_product_code', 'title', 'price_including_tax', 
     'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
 
-    with open (input('Nomme le fichier CSV: '), 'w') as file:
+    with open (file_name, 'w') as file:
         writer_csv = csv.writer(file, delimiter=',')
         writer_csv.writerow(ligne_en_tete)
         writer_csv.writerow(program)
 
 if __name__ =='__main__':
     result_request = fetch("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
-    recups = recup(result_request)
-    info_page_csv(recups)
+    recup_program = recup(result_request)
+    file_name = input("nomme le fichier: ")
+    creating_csv_file(recup_program, file_name)
